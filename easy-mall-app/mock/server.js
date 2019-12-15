a78abc595c3534971c593485f0f8f880a280a5f3
zouyuou-home <hellozouyou@gmail.com>
2019-12-15 19:37:07 +0800

    补充购物车页面

--- easy-mall-app/mock/server.js
+++ easy-mall-app/mock/server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -12,6 +14,18 @@ require('./router')(app)
 //  指定端口
 const PORT = 8080;
 
+// 获取本机的局域网IP
+var os = require('os'),
+    iptable = {},
+    ifaces = os.networkInterfaces();
+for (var dev in ifaces) {
+    ifaces[dev].forEach(function (details, alias) {
+        if (details.family == 'IPv4') {
+            iptable[dev + (alias ? ':' + alias : '')] = details.address;
+        }
+    });
+}
+
 app.listen(PORT, () => {
-    console.log(`服务器启动，运行为http://localhost:${PORT}`);
+    console.log(`服务器启动，运行为http://${iptable["WLAN:1"]}:${PORT}`);
 });
